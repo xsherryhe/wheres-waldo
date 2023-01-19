ccb8e0d4443cc70805825f7c322bdfa9b4418d0d
Sherry He <xsherryhe@gmail.com>
2023-01-19 14:12:51 -0500

    Integrated basic fetch and usage of back-end data

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,6 +1,5 @@
 import { useState, useEffect, useRef } from 'react';
 import '../styles/Game.css';
-import dummy from '../images/dummy.jpg';
 
 import Square from './Square';
 import SelectForm from './SelectForm';
@@ -11,6 +10,7 @@ export default function Game() {
   const [targets, setTargets] = useState(null);
   const [active, setActive] = useState(null);
 
+  const serverDomainRef = useRef('http://localhost:3000');
   const gameRef = useRef();
 
   useEffect(() => {
@@ -22,37 +22,58 @@ export default function Game() {
   }, [gameRef]);
 
   useEffect(() => {
-    setImage(dummy);
-    setGrid(
-      [...new Array(10)].map((_, i) =>
-        [...new Array(9)].map((_, j) => `${i},${j}`)
-      )
-    );
-    setTargets(['Waiter', 'Solid brown lollipop', 'Glittery lollipop']);
+    async function setGame() {
+      const response = await fetch(`${serverDomainRef.current}/images/3`, {
+        mode: 'cors',
+      });
+      const data = await response.json();
+      console.log(data);
+      setImage(`${serverDomainRef.current}/image_files/${data.file}`);
+      setGrid(
+        [...new Array(10)].map((_, i) =>
+          [...new Array(21)].map((_, j) => `${i},${j}`)
+        )
+      );
+      setTargets(data.targets);
+    }
+    setGame();
   }, []);
 
   if (!image) return <div>Loading...</div>;
 
   return (
-    <div
-      className="game"
-      style={{
-        backgroundImage: `url(${image})`,
-        gridTemplate: `repeat(${grid.length}, 1fr) / repeat(${grid[0].length}, 1fr)`,
-      }}
-      ref={gameRef}
-    >
-      {grid.map((row) =>
-        row.map((id) => (
-          <div key={id}>
-            <Square
-              active={active === id}
-              setActive={() => setActive(id)}
-            />
-            {active === id && <SelectForm options={targets} />}
-          </div>
-        ))
-      )}
+    <div className="game">
+      <div className="game-panel">
+        <h2 className="instructions">Where are they?</h2>
+        <div className="game-targets">
+          {targets.map(({ id, name, file }) => (
+            <div key={id}>
+              <img
+                src={`${serverDomainRef.current}/image_files/${file}`}
+                alt=""
+              />
+              {name}
+            </div>
+          ))}
+        </div>
+      </div>
+      <div
+        className="game-image"
+        style={{
+          backgroundImage: `url(${image})`,
+          gridTemplate: `repeat(${grid.length}, 1fr) / repeat(${grid[0].length}, 1fr)`,
+        }}
+        ref={gameRef}
+      >
+        {grid.map((row) =>
+          row.map((id) => (
+            <div key={id}>
+              <Square active={active === id} setActive={() => setActive(id)} />
+              {active === id && <SelectForm options={targets} squareId={id} />}
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 }
--- src/components/SelectForm.js
+++ src/components/SelectForm.js
@@ -1,13 +1,15 @@
 import '../styles/SelectForm.css';
 
-export default function SelectForm({ options }) {
+export default function SelectForm({ options, squareId }) {
+  // onChange of select -- if select has value, then
+  // send select value with squareId and display checking... message
   return (
     <form className="select" action="">
       <select name="selection" id="selection">
         <option value="">-- Who or what is here? --</option>
-        {options.map((option) => (
-          <option key={option} id={option}>
-            {option}
+        {options.map(({ id, name }) => (
+          <option key={id} id={id}>
+            {name}
           </option>
         ))}
       </select>
